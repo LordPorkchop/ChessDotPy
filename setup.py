804b69eb31b4cbb7218e6aa5ea0edf14519a4b34
Lord Porkchop <149256533+LordPorkchop@users.noreply.github.com>
2025-04-14 13:55:57 +0200

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+os.system('pip install setuptools')  # Ensure setuptools is installed
+from setuptools import setup, find_packages
+
+
+def get_version():
+    with open("chessdotpy/version.py") as f:
+        for line in f:
+            if line.startswith("__version__"):
+                return line.split("=")[1].strip().strip('"')
+    raise RuntimeError("Unable to find version string.")
+
+# Read dependencies from requirements.txt
+
+
+def parse_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+setup(
+    name="chessdotpy",  # Replace with your package name
+    version=get_version(),  # Dynamically get the version from version.py
+    author="Lord Porkchop",  # Replace with your name
+    # Short description of your package
+    description="Chess in Python, featuring Stockfish and chess.com / Lichess API support",
+    long_description=open('README.md').read(),  # If you have a README.md
+    # Assuming you use markdown for README
+    long_description_content_type="text/markdown",
+    url="https://github.com/LordPorkchop/chessdotpy",  # GitHub URL
+    packages=find_packages(),  # Automatically find all packages
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Windows :: Windows 10+",
+    ],
+    python_requires=">=3.6",  # Define the minimum Python version
+    install_requires=parse_requirements(),   # Dynamically get requirements from requirements.txt
+    include_package_data=True,  # Include files specified in MANIFEST.in
+)
